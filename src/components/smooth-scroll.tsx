ed3ce065840f0c60f4694e9970b48949f2e55461
Sohan Sijoy <sohan.sijoy@gmail.com>
2026-02-15 11:20:28 +0530

    Fix TypeScript implicit any in smooth-scroll

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -11,7 +11,7 @@ export default function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
       smooth: true,
